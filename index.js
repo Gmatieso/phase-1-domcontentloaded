1b020867ae6d8978ba63c13b50f6cf3f49fd9534
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-08-28 18:06:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+  });
+  
+ function updateDOM(){
+    const item = document.getElementById('text')
+   return  item.textContent = "This is really cool!"
+ }
+  
